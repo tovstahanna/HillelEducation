f13960bfc0763a6ccc0ea2fc3f275a58b28fc6f8
tovstahanna <tovsta.hanna@gmail.com>
2023-02-25 13:05:56 +0100

    add pro dz2

---
+++ pro/dz2/script.js
@@ -0,0 +1,11 @@
+let summ, minus, multi, division
+alert('Hello!')
+const firtsNumber = prompt('What is the first number?');
+const secondNumber = prompt('What is the second number?');
+
+sum = firtsNumber + secondNumber
+minus = firtsNumber - secondNumber
+multi = firtsNumber * secondNumber
+division = firtsNumber / secondNumber
+
+alert('Calculations are finished!\nSum: ' + firtsNumber + ' + ' + secondNumber + ' = ' + sum + '\nDiff: ' + firtsNumber + ' - ' + secondNumber + ' = ' + minus + '\nMult: ' + firtsNumber + ' * ' + secondNumber + ' = ' + multi + '\nDiv: ' + firtsNumber + ' / ' + secondNumber + ' = ' + division)
