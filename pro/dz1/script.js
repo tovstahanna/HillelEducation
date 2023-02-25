182c7e8c649d3bbef5bdd0aeac813231a765031d
tovstahanna <tovsta.hanna@gmail.com>
2023-02-25 12:42:45 +0100

    add pro dz1

---
+++ pro/dz1/script.js
@@ -0,0 +1,7 @@
+const currentYear = 2023
+alert('Hello!')
+const userName = prompt('What is your name?');
+const birthYear = prompt('What is your birth year?');
+const country = prompt('Where are you from?');
+
+alert('Nice to meet you, ' + userName + '! You are ' + (currentYear - birthYear)  + ' years old! You are from ' + country +'!')
